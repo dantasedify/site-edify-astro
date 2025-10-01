8ee21598ebf3c97fbf1db553be43c503a5131c1c
Allan Dantas <dantas.alves.allan@gmail.com>
2025-10-01 15:49:30 -0300

    feat: Customiza componente ExtendedFeature

--- src/components/functional/ExtendedFeatureAccordion.jsx
+++ src/components/functional/ExtendedFeatureAccordion.jsx
@@ -4,16 +4,17 @@ import ExtendedFeatureCard from "./common/ExtendedFeatureCard";
 const ExtendedFeatureAccordion = ({ data }) => {
   const [openedCard, setOpenedCard] = useState(data[0]?.id);
   return (
-    <div className="zubuz-accordion-wrap" id="zubuz-accordion">
+    <div className="zubuz-accordion-wrap row" id="zubuz-accordion">
       {data.map((card) => (
-        <ExtendedFeatureCard
-          key={card?.id}
-          title={card?.data?.title}
-          desc={card?.data?.desc}
-          id={card?.id}
-          openedCard={openedCard}
-          onClick={() => setOpenedCard(card?.id)}
-        />
+        <div className="col-12 col-md-6" key={card?.id}>
+          <ExtendedFeatureCard
+            title={card?.data?.title}
+            desc={card?.data?.desc}
+            id={card?.id}
+            openedCard={openedCard}
+            onClick={() => setOpenedCard(card?.id)}
+          />
+        </div>
       ))}
     </div>
   );
